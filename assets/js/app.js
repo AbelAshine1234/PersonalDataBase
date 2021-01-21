e2f8aa55a4aa2a7405d583a26a4e73610842d6e1
AbelAshine1234 <abelashine80@gmail.com>
2021-01-21 13:57:50 +0300

    commit three

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+// Declaring variables 
+// You can check this Variables are Available or not under the window object
+var firstName; 
+var lastName; 
+var age; 
+var job;
+// Receive the values from input 
+firstName = prompt("Enter Your First Name");
+lastName = prompt("Enter Your Last Name");
+job = prompt("What is Your Profession ?")
+age = prompt("Enter Your Age");
+// Display  the result on console from input 
+console.log("Here is your Profile ")
+console.log("Full Name: " + firstName + " "+lastName);
+console.log("Profession : " + job);
+console.log("Age : " + age + " " + "years old");
