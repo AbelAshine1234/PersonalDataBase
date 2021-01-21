fda0acfe4396d89140ae05cce82b851a5d51310e
AbelAshine1234 <abelashine80@gmail.com>
2021-01-21 14:54:48 +0300

    commit five

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,26 +3,30 @@
 var firstName; 
 var lastName; 
 
-var age; 
+var birthYear; 
 var job;
+
+var weight;
+var height;
 
 // Receive the values from input 
 firstName = prompt("Enter Your First Name");
 lastName = prompt("Enter Your Last Name");
 
 job = prompt("What is Your Profession ?")
-age = prompt("Enter Your Age");
+birthYear = prompt("Enter Your Birth Year");
 
 // Display  the result on console from input 
 console.log("Here is your Profile ");
 console.log("Full Name: " + firstName + " "+lastName);
 console.log("Profession : " + job);
-console.log("Age : " + age + " " + "years old");
+console.log("Birth Year: " + birthYear + " " + "");
 
 
-let tempAge;
+let tempAge ;
+tempAge = parseInt(ageCalc(birthYear));
+
 let isEligibleVote = tempAge;
-tempAge = parseInt(age);
 if (tempAge >= 18){
     isEligibleVote  = true;
 }
@@ -31,3 +35,35 @@ else{
 }
 
 console.log("is Eligible to Vote"+isEligibleVote)
+
+
+
+
+// 
+let familyMember = new Array();
+let noOfFamily;
+
+noOfFamily = prompt("No Of Family");
+
+for (let i = 0; i< parseInt(noOfFamily); i++) {
+    familyMember[i] =  prompt("your family members "+(i+1));
+
+}
+
+
+console.log("Family Member");
+
+familyMember.forEach(function(familyMember,index) {
+    console.log("Family Member  " + (index + 1) + " : " + familyMember);
+ });
+
+function ageCalc(birthYear){
+    return new Date().getFullYear() - birthYear;
+
+}
+
+let calcBmi = function(weight,height){
+    return weight / Math.pow(height);
+
+}
+console.log(calcBmi);
