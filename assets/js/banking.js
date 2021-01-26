bc9f5be96297b71eba7a687f0b1717be9e57a1f5
AbelAshine1234 <abelashine80@gmail.com>
2021-01-27 00:48:41 +0300

    starting the banking system

---
+++ assets/js/banking.js
@@ -0,0 +1,19 @@
+var accoutnNo;
+var ballance;
+var accountOne;
+var accountTwo;
+var accountThree;
+var accountFOur;
+
+function Deposit(){
+    
+}
+function WithDrawal(){
+    
+}
+function Balance(){
+    
+}
+function Transfer(){
+    
+}
